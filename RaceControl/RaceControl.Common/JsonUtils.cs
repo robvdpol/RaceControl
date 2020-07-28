36662dd31fa4b2b371f6154f58ed710e73dfa063
robvdpol <rob_van_der_pol@hotmail.com>
2020-07-28 15:08:23 +0200

    .

---
+++ RaceControl/RaceControl.Common/JsonUtils.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace RaceControl.Common
+{
+    public static class JsonUtils
+    {
+        public static string GetJsonPropertyName<T>(this Expression<Func<T, object>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+
+            if (memberExpression == null && expression.Body is UnaryExpression unaryExpression)
+            {
+                memberExpression = unaryExpression.Operand as MemberExpression;
+            }
+
+            if (memberExpression == null)
+            {
+                throw new InvalidOperationException("Expression must be a member expression");
+            }
+
+            var attribute = memberExpression.Member.GetAttribute<JsonPropertyAttribute>();
+
+            if (attribute == null)
+            {
+                throw new ArgumentException("Property must have a JsonPropertyAttribute");
+            }
+
+            return attribute.PropertyName;
+        }
+
+        public static T GetAttribute<T>(this ICustomAttributeProvider provider) where T : Attribute
+        {
+            return provider.GetCustomAttributes(typeof(T), true).FirstOrDefault() as T;
+        }
+    }
+}
---
+++ RaceControl/RaceControl.Common/StringUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace RaceControl.Common
+{
+    public static class StringUtils
+    {
+        public static string GetUID(this string url)
+        {
+            if (url.EndsWith("/"))
+            {
+                url = url.Substring(0, url.Length - 1);
+            }
+
+            var idx = url.LastIndexOf('/');
+
+            return url.Substring(idx + 1);
+        }
+    }
+}
