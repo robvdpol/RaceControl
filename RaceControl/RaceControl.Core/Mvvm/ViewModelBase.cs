8592592c008ce1fb67cf9b00f4b842182d508574
robvdpol <rob_van_der_pol@hotmail.com>
2020-08-12 22:05:35 +0200

    Prevent keyboard input when busy

--- RaceControl/RaceControl.Core/Mvvm/ViewModelBase.cs
+++ RaceControl/RaceControl.Core/Mvvm/ViewModelBase.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows.Input;
 
 namespace RaceControl.Core.Mvvm
 {
@@ -6,10 +8,32 @@ namespace RaceControl.Core.Mvvm
     {
         private bool _isBusy;
 
+        private ICommand _previewKeyDownCommand;
+        private ICommand _keyDownCommand;
+
         public bool IsBusy
         {
             get => _isBusy;
             set => SetProperty(ref _isBusy, value);
+        }
+
+        public ICommand PreviewKeyDownCommand => _previewKeyDownCommand ??= new DelegateCommand<KeyEventArgs>(PreviewKeyDownExecute);
+        public ICommand KeyDownCommand => _keyDownCommand ??= new DelegateCommand<KeyEventArgs>(KeyDownExecute);
+
+        private void PreviewKeyDownExecute(KeyEventArgs args)
+        {
+            if (IsBusy)
+            {
+                args.Handled = true;
+            }
+        }
+
+        private void KeyDownExecute(KeyEventArgs args)
+        {
+            if (IsBusy)
+            {
+                args.Handled = true;
+            }
         }
     }
 }
