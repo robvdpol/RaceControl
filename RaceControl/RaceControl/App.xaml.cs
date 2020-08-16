11f0a847e479209ceb31b1319c55780888c90705
robvdpol <rob_van_der_pol@hotmail.com>
2020-08-16 22:02:31 +0200

    Logging max file size

--- RaceControl/RaceControl/App.xaml.cs
+++ RaceControl/RaceControl/App.xaml.cs
@@ -70,7 +70,10 @@ namespace RaceControl
             var logfile = new FileTarget("logfile")
             {
                 FileName = "RaceControl.log",
-                Layout = Layout.FromString("${longdate} ${uppercase:${level}} ${message}${onexception:inner=${newline}${exception:format=tostring}}")
+                Layout = Layout.FromString("${longdate} ${uppercase:${level}} ${message}${onexception:inner=${newline}${exception:format=tostring}}"),
+                ArchiveAboveSize = 10 * 1024 * 1024,
+                ArchiveNumbering = ArchiveNumberingMode.Rolling,
+                MaxArchiveFiles = 1
             };
             config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
             LogManager.Configuration = config;
