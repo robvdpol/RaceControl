5adc3744cfaa45d9bb572edc9deb1c35fb5887c3
robvdpol <rob_van_der_pol@hotmail.com>
2020-08-17 19:34:33 +0200

    Added check for active F1TV subscription

--- RaceControl/Services/RaceControl.Services/F1TV/AuthorizationService.cs
+++ RaceControl/Services/RaceControl.Services/F1TV/AuthorizationService.cs
@@ -1,6 +1,7 @@
 ﻿using RaceControl.Services.Interfaces;
 using RaceControl.Services.Interfaces.F1TV;
 using RaceControl.Services.Interfaces.F1TV.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -39,6 +40,11 @@ namespace RaceControl.Services.F1TV
         public async Task<TokenResponse> LoginAsync(string login, string password)
         {
             var authResponse = await AuthenticateAsync(login, password);
+
+            if (authResponse.Data.SubscriptionStatus != "active")
+            {
+                throw new Exception("An active F1TV subscription is required.");
+            }
 
             var tokenRequest = new TokenRequest
             {
