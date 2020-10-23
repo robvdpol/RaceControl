7eb1dc9d68ab369f5df2456dd8c27b6a780a3bda
Rob <rob_van_der_pol@hotmail.com>
2020-10-23 12:57:32 +0200

    Removed streamlink 1.6

--- RaceControl/RaceControl.Streamlink/Streamlink/packages/streamlink/plugin/__init__.py
+++
@@ -1,6 +0,0 @@
-from .plugin import Plugin
-from ..exceptions import PluginError
-from ..options import Options as PluginOptions
-from ..options import Arguments as PluginArguments, Argument as PluginArgument
-
-__all__ = ["Plugin", "PluginError", "PluginOptions", "PluginArguments", "PluginArgument"]
