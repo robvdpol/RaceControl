485ff49258ada2d97a47235648dd0ca9b3d11df6
robvdpol <rob_van_der_pol@hotmail.com>
2021-04-17 15:38:42 +0200

    Improved error handling in internal player when stream url could not be requested

--- RaceControl/RaceControl/ViewModels/VideoDialogViewModel.cs
+++ RaceControl/RaceControl/ViewModels/VideoDialogViewModel.cs
@@ -146,7 +146,7 @@ namespace RaceControl.ViewModels
                 StartupLocation = WindowStartupLocation.CenterScreen;
             }
 
-            InitializeAsync().Await(InitializeCompleted, InitializeError);
+            InitializeAsync().Await(InitializeCompleted, InitializeError, true);
         }
 
         public override void OnDialogClosed()
@@ -445,6 +445,7 @@ namespace RaceControl.ViewModels
         private void InitializeError(Exception ex)
         {
             base.OnDialogOpened(null);
+            RaiseRequestClose();
             HandleCriticalError(ex);
         }
 
@@ -470,6 +471,12 @@ namespace RaceControl.ViewModels
             // DASH works best for live streams, HLS for replays
             var streamType = _settings.GetStreamType(PlayableContent.IsLive ? StreamTypeKeys.BigScreenDash : StreamTypeKeys.BigScreenHls);
             var streamUrl = await _apiService.GetTokenisedUrlAsync(_subscriptionToken, streamType, PlayableContent);
+
+            if (string.IsNullOrWhiteSpace(streamUrl))
+            {
+                throw new Exception("An error occurred while retrieving the stream URL.");
+            }
+
             await MediaPlayer.StartPlaybackAsync(streamUrl, DialogSettings.VideoQuality);
         }
 
