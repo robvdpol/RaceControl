34cf242091871c637ebfe9feea9dadb730ea38c8
robvdpol <rob_van_der_pol@hotmail.com>
2021-05-13 17:17:01 +0200

    Removed MPV project, moved Mpv installation to Libs folder, added 32-bit version

---
+++ Libs/x86/Mpv/updater.bat
@@ -0,0 +1,15 @@
+@echo OFF
+:: This batch file exists to run updater.ps1 without hassle
+pushd %~dp0
+if exist "%~dp0\installer\updater.ps1" (
+    set updater_script="%~dp0\installer\updater.ps1"
+) else (
+    set updater_script="%~dp0\updater.ps1"
+)
+powershell -noprofile -nologo -executionpolicy bypass -File %updater_script%
+
+:: After update, updater.ps1 should not in same folder as mpv.exe
+if exist "%~dp0\installer\updater.ps1" if exist "%~dp0\updater.ps1" (
+    del "%~dp0\updater.ps1"
+)
+@pause
