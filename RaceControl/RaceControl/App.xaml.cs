b69fa8aa2055f04e8e9a2b3fee81cd45cd33c6cf
robvdpol <rob_van_der_pol@hotmail.com>
2021-11-01 08:58:25 +0100

    Changed probsize/analyzeduration

--- RaceControl/RaceControl/App.xaml.cs
+++ RaceControl/RaceControl/App.xaml.cs
@@ -122,8 +122,8 @@ namespace RaceControl
         private static Player CreateFlyleafPlayer()
         {
             var config = new Config();
-            config.Demuxer.FormatOpt.Add("probesize", (50 * 1024 * 1024).ToString());
-            config.Demuxer.FormatOpt.Add("analyzeduration", (10 * 1000 * 1000).ToString());
+            config.Demuxer.FormatOpt.Add("probesize", (5 * 1024 * 1024).ToString());
+            config.Demuxer.FormatOpt.Add("analyzeduration", (5 * 1000 * 1000).ToString());
             config.Demuxer.BufferDuration = TimeSpan.FromSeconds(30).Ticks;
 
             return new(config);
