42059def8b03cc69dd772c994040747658069fe0
robvdpol <rob_van_der_pol@hotmail.com>
2021-11-01 10:14:24 +0100

    .

--- RaceControl/RaceControl/ViewModels/VideoDialogViewModel.cs
+++ RaceControl/RaceControl/ViewModels/VideoDialogViewModel.cs
@@ -452,7 +452,7 @@ namespace RaceControl.ViewModels
 
         private void OnCloseAll(ContentType? contentType)
         {
-            var delay = TimeSpan.FromMilliseconds((_identifier - 1) * 200);
+            var delay = TimeSpan.FromMilliseconds((_identifier - 1) * 150);
             DelayAsync(delay).Await(() => CloseWindowCommand.TryExecute(), null, true);
         }
 
