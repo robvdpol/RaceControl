1d09cfc50f65e246540a882552c3df672af67eb0
robvdpol <rob_van_der_pol@hotmail.com>
2021-11-06 20:50:44 +0100

    Disabled seeking / syncing for live sessions

--- RaceControl/RaceControl/ViewModels/VideoDialogViewModel.cs
+++ RaceControl/RaceControl/ViewModels/VideoDialogViewModel.cs
@@ -95,8 +95,8 @@ namespace RaceControl.ViewModels
         public ICommand TogglePauseCommand => _togglePauseCommand ??= new DelegateCommand(TogglePauseExecute).ObservesCanExecute(() => MediaPlayer.IsStarted);
         public ICommand TogglePauseAllCommand => _togglePauseAllCommand ??= new DelegateCommand(TogglePauseAllExecute);
         public ICommand ToggleMuteCommand => _toggleMuteCommand ??= new DelegateCommand<bool?>(ToggleMuteExecute).ObservesCanExecute(() => MediaPlayer.IsStarted);
-        public ICommand FastForwardCommand => _fastForwardCommand ??= new DelegateCommand<int?>(FastForwardExecute).ObservesCanExecute(() => MediaPlayer.IsStarted);
-        public ICommand SyncSessionCommand => _syncSessionCommand ??= new DelegateCommand(SyncSessionExecute).ObservesCanExecute(() => MediaPlayer.IsStarted);
+        public ICommand FastForwardCommand => _fastForwardCommand ??= new DelegateCommand<int?>(FastForwardExecute, CanFastForwardExecute).ObservesProperty(() => MediaPlayer.IsStarted).ObservesProperty(() => PlayableContent);
+        public ICommand SyncSessionCommand => _syncSessionCommand ??= new DelegateCommand(SyncSessionExecute, CanSyncSessionExecute).ObservesProperty(() => MediaPlayer.IsStarted).ObservesProperty(() => PlayableContent);
         public ICommand ShowMainWindowCommand => _showMainWindowCommand ??= new DelegateCommand(ShowMainWindowExecute);
         public ICommand ToggleRecordingCommand => _toggleRecordingCommand ??= new DelegateCommand(ToggleRecordingExecute).ObservesCanExecute(() => MediaPlayer.IsStarted);
         public ICommand ToggleFullScreenCommand => _toggleFullScreenCommand ??= new DelegateCommand<long?>(ToggleFullScreenExecute);
@@ -293,12 +293,22 @@ namespace RaceControl.ViewModels
             MediaPlayer.ToggleMute(mute);
         }
 
+        private bool CanFastForwardExecute(int? seconds)
+        {
+            return MediaPlayer.IsStarted && !PlayableContent.IsLive;
+        }
+
         private void FastForwardExecute(int? seconds)
         {
             if (seconds.HasValue)
             {
                 MediaPlayer.FastForward(seconds.Value);
             }
+        }
+
+        private bool CanSyncSessionExecute()
+        {
+            return MediaPlayer.IsStarted && !PlayableContent.IsLive;
         }
 
         private void SyncSessionExecute()
