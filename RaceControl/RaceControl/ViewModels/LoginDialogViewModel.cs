73330819b6c9eb82cd090e9d27d482fed0b13c37
robvdpol <rob_van_der_pol@hotmail.com>
2022-04-09 14:03:53 +0200

    Improved login dialog

--- RaceControl/RaceControl/ViewModels/LoginDialogViewModel.cs
+++ RaceControl/RaceControl/ViewModels/LoginDialogViewModel.cs
@@ -6,10 +6,7 @@ namespace RaceControl.ViewModels;
 
 public class LoginDialogViewModel : DialogViewModelBase
 {
-    private ICommand _initializationCompletedCommand;
     private ICommand _sourceChangedCommand;
-
-    private WebView2 _webView;
 
     public LoginDialogViewModel(ILogger logger) : base(logger)
     {
@@ -17,35 +14,25 @@ public class LoginDialogViewModel : DialogViewModelBase
 
     public override string Title => "Login";
 
-    public ICommand InitializationCompletedCommand => _initializationCompletedCommand ??= new DelegateCommand<WebView2>(InitializationCompletedExecute);
-    public ICommand SourceChangedCommand => _sourceChangedCommand ??= new DelegateCommand(SourceChangedExecute);
+    public ICommand SourceChangedCommand => _sourceChangedCommand ??= new DelegateCommand<WebView2>(SourceChangedExecute);
 
-    private void InitializationCompletedExecute(WebView2 webView)
+    private void SourceChangedExecute(WebView2 webView)
     {
-        _webView = webView;
-        _webView.CoreWebView2.CookieManager.DeleteAllCookies();
-        _webView.Source = new Uri("https://account.formula1.com/#/en/login");
-    }
-
-    private void SourceChangedExecute()
-    {
-        FindSubscriptionTokenAsync().Await(LoginSuccess, LoginError, true);
+        FindSubscriptionTokenAsync(webView).Await(LoginSuccess, LoginError, true);
     }
 
     private void LoginSuccess(string subscriptionToken)
     {
-        if (string.IsNullOrWhiteSpace(subscriptionToken))
+        if (!string.IsNullOrWhiteSpace(subscriptionToken))
         {
-            return;
+            Logger.Info("Login successful.");
+
+            RaiseRequestClose(ButtonResult.OK, new DialogParameters
+            {
+                { ParameterNames.SubscriptionToken, subscriptionToken },
+                { ParameterNames.SubscriptionStatus, "unknown" }
+            });
         }
-
-        Logger.Info("Login successful.");
-
-        RaiseRequestClose(ButtonResult.OK, new DialogParameters
-        {
-            { ParameterNames.SubscriptionToken, subscriptionToken },
-            { ParameterNames.SubscriptionStatus, "unknown" }
-        });
     }
 
     private void LoginError(Exception ex)
@@ -53,9 +40,9 @@ public class LoginDialogViewModel : DialogViewModelBase
         Logger.Error(ex, "Login error.");
     }
 
-    private async Task<string> FindSubscriptionTokenAsync()
+    private static async Task<string> FindSubscriptionTokenAsync(WebView2 webView)
     {
-        var cookies = await _webView.CoreWebView2.CookieManager.GetCookiesAsync(Constants.ApiEndpointUrl);
+        var cookies = await webView.CoreWebView2.CookieManager.GetCookiesAsync(Constants.ApiEndpointUrl);
         var loginCookie = cookies.FirstOrDefault(cookie => string.Equals(cookie.Name, "login-session", StringComparison.OrdinalIgnoreCase));
 
         if (loginCookie != null)
--- RaceControl/RaceControl/Views/LoginDialog.xaml.cs
+++ RaceControl/RaceControl/Views/LoginDialog.xaml.cs
@@ -11,11 +11,15 @@ public partial class LoginDialog
 
     private void LoginDialogLoaded(object sender, RoutedEventArgs e)
     {
-        var userDataFolder = FolderUtils.GetWebView2UserDataPath();
+        InitializeWebViewAsync().Await();
+    }
 
-        CoreWebView2Environment.CreateAsync(userDataFolder: userDataFolder).Await(environment =>
-        {
-            WebView2.EnsureCoreWebView2Async(environment).Await();
-        });
+    private async Task InitializeWebViewAsync()
+    {
+        var userDataFolder = FolderUtils.GetWebView2UserDataPath();
+        var environment = await CoreWebView2Environment.CreateAsync(userDataFolder: userDataFolder);
+        await WebView2.EnsureCoreWebView2Async(environment);
+        WebView2.CoreWebView2.CookieManager.DeleteAllCookies();
+        WebView2.Source = new Uri("https://account.formula1.com/#/en/login");
     }
 }
