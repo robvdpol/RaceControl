79ce1e6ba5ecfd55372d3ef9c55a6a5a6964ce1d
robvdpol <rob_van_der_pol@hotmail.com>
2022-04-09 14:11:39 +0200

    Login dialog stuff...

--- RaceControl/RaceControl/Views/LoginDialog.xaml.cs
+++ RaceControl/RaceControl/Views/LoginDialog.xaml.cs
@@ -4,14 +4,17 @@ namespace RaceControl.Views;
 
 public partial class LoginDialog
 {
-    public LoginDialog()
+    private readonly ILogger _logger;
+
+    public LoginDialog(ILogger logger)
     {
         InitializeComponent();
+        _logger = logger;
     }
 
     private void LoginDialogLoaded(object sender, RoutedEventArgs e)
     {
-        InitializeWebViewAsync().Await();
+        InitializeWebViewAsync().Await(InitializeWebViewSuccess, InitializeWebViewFailed, true);
     }
 
     private async Task InitializeWebViewAsync()
@@ -19,7 +22,16 @@ public partial class LoginDialog
         var userDataFolder = FolderUtils.GetWebView2UserDataPath();
         var environment = await CoreWebView2Environment.CreateAsync(userDataFolder: userDataFolder);
         await WebView2.EnsureCoreWebView2Async(environment);
+    }
+
+    private void InitializeWebViewSuccess()
+    {
         WebView2.CoreWebView2.CookieManager.DeleteAllCookies();
         WebView2.Source = new Uri("https://account.formula1.com/#/en/login");
+    }
+
+    private void InitializeWebViewFailed(Exception ex)
+    {
+        _logger.Error(ex, "An error occurred while initializing webview.");
     }
 }
